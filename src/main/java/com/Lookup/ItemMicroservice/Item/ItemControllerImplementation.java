569a45283bf95a2ed364dcaf0909187cd89e4415
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2024-11-04 11:42:38 -0500

    h

--- src/main/java/com/Lookup/ItemMicroservice/Item/ItemControllerImplementation.java
+++ src/main/java/com/Lookup/ItemMicroservice/Item/ItemControllerImplementation.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/item")
 public class ItemControllerImplementation implements ItemController {
     private final ItemService itemService;
 
